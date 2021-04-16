3ff8f78b832b830f2e447f361f5eddccf08b7433
Fabiitch <fabien.occelli@hotmail.fr>
2021-04-16 14:58:18 +0200

    Vectors len(), len2() test

--- gdx/test/com/badlogic/gdx/math/Vector2Test.java
+++ gdx/test/com/badlogic/gdx/math/Vector2Test.java
@@ -6,33 +6,65 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class Vector2Test {
-	@Test
-	public void testToString () {
-		assertEquals("(-5.0,42.00055)", new Vector2(-5f, 42.00055f).toString());
-	}
+    @Test
+    public void testToString() {
+        assertEquals("(-5.0,42.00055)", new Vector2(-5f, 42.00055f).toString());
+    }
 
-	@Test
-	public void testFromString () {
-		assertEquals(new Vector2(-5f, 42.00055f), new Vector2().fromString("(-5,42.00055)"));
-	}
+    @Test
+    public void testFromString() {
+        assertEquals(new Vector2(-5f, 42.00055f), new Vector2().fromString("(-5,42.00055)"));
+    }
 
-	@Test
-	public void testAngle() {
-		assertEquals(270f, new Vector2(0, -1f).angleDeg(), MathUtils.FLOAT_ROUNDING_ERROR);
-	}
+    @Test
+    public void testAngle() {
+        assertEquals(270f, new Vector2(0, -1f).angleDeg(), MathUtils.FLOAT_ROUNDING_ERROR);
+    }
 
-	@Test
-	public void testAngleRelative() {
-		assertEquals(270f, new Vector2(0, -1f).angleDeg(Vector2.X), MathUtils.FLOAT_ROUNDING_ERROR);
-	}
+    @Test
+    public void testAngleRelative() {
+        assertEquals(270f, new Vector2(0, -1f).angleDeg(Vector2.X), MathUtils.FLOAT_ROUNDING_ERROR);
+    }
 
-	@Test
-	public void testAngleRad() {
-		assertEquals(- MathUtils.HALF_PI, new Vector2(0, -1f).angleRad(), MathUtils.FLOAT_ROUNDING_ERROR);
-	}
+    @Test
+    public void testAngleRad() {
+        assertEquals(-MathUtils.HALF_PI, new Vector2(0, -1f).angleRad(), MathUtils.FLOAT_ROUNDING_ERROR);
+    }
 
-	@Test
-	public void testAngleRadRelative() {
-		assertEquals(- MathUtils.HALF_PI, new Vector2(0, -1f).angleRad(Vector2.X), MathUtils.FLOAT_ROUNDING_ERROR);
-	}
+    @Test
+    public void testAngleRadRelative() {
+        assertEquals(-MathUtils.HALF_PI, new Vector2(0, -1f).angleRad(Vector2.X), MathUtils.FLOAT_ROUNDING_ERROR);
+    }
+
+
+    @Test
+    public void testLenLen2() {
+        {
+            for (int i = 0; i < 1000; i++) {
+                float random1 = MathUtils.random(100f);
+                float random2 = MathUtils.random(100f);
+
+                Vector2 v2 = new Vector2(random1, random2);
+                Vector2 v2Nor = new Vector2(v2);
+                v2Nor.nor();
+                float len = v2Nor.len();
+                float len2 = v2Nor.len2();
+                System.out.println("=========");
+                System.out.println("V2 = " + v2.toString() + "   nor=" + v2Nor.toString());
+
+                if (len != 1f) {
+                    System.out.println("error : len()=" + len + "  != 1f");
+                }
+                if (len2 != 1f) {
+                    System.out.println("error : len2()=" + len2 + " != 1f");
+                }
+                if (!MathUtils.isEqual(len, 1f)) {
+                    System.out.println("error : len()=" + len + " != 1f with FLOAT_ROUNDING_ERROR");
+                }
+                if (!MathUtils.isEqual(len2, 1f)) {
+                    System.out.println("error : len2()=" + len2 + " != 1f for  FLOAT_ROUNDING_ERROR tolerance ");
+                }
+            }
+        }
+    }
 }
--- gdx/test/com/badlogic/gdx/math/Vector3Test.java
+++ gdx/test/com/badlogic/gdx/math/Vector3Test.java
@@ -6,13 +6,45 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class Vector3Test {
-	@Test
-	public void testToString () {
-		assertEquals("(-5.0,42.00055,44444.32)", new Vector3(-5f, 42.00055f, 44444.32f).toString());
-	}
+    @Test
+    public void testToString() {
+        assertEquals("(-5.0,42.00055,44444.32)", new Vector3(-5f, 42.00055f, 44444.32f).toString());
+    }
 
-	@Test
-	public void testFromString () {
-		assertEquals(new Vector3(-5f, 42.00055f, 44444.32f), new Vector3().fromString("(-5,42.00055,44444.32)"));
-	}
+    @Test
+    public void testFromString() {
+        assertEquals(new Vector3(-5f, 42.00055f, 44444.32f), new Vector3().fromString("(-5,42.00055,44444.32)"));
+    }
+
+
+    @Test
+    public void testLenLen2() {
+        {
+            for (int i = 0; i < 1000; i++) {
+                float random1 = MathUtils.random(100f);
+                float random2 = MathUtils.random(100f);
+                float random3 = MathUtils.random(100f);
+                Vector3 v3 = new Vector3(random1, random2,random3);
+                Vector3 v3Nor = new Vector3(v3);
+                v3Nor.nor();
+                float len = v3Nor.len();
+                float len2 = v3Nor.len2();
+                System.out.println("=========");
+                System.out.println("V2 = " + v3.toString() + "   nor=" + v3Nor.toString());
+
+                if (len != 1f) {
+                    System.out.println("error : len()=" + len + "  != 1f");
+                }
+                if (len2 != 1f) {
+                    System.out.println("error : len2()=" + len2 + " != 1f");
+                }
+                if (!MathUtils.isEqual(len, 1f)) {
+                    System.out.println("error : len()=" + len + " != 1f with FLOAT_ROUNDING_ERROR");
+                }
+                if (!MathUtils.isEqual(len2, 1f)) {
+                    System.out.println("error : len2()=" + len2 + " != 1f for  FLOAT_ROUNDING_ERROR tolerance ");
+                }
+            }
+        }
+    }
 }
